36c007e83e446e9eaa5101e535cf8df531ec22f6
Tim Golen <tgolen@gmail.com>
2022-09-17 08:52:03 -0600

    Parse all files and output them

--- scripts/generateSite.js
+++ scripts/generateSite.js
@@ -4,28 +4,44 @@ import fs from 'fs';
 import siteData from '../data/site.json' assert {type: 'json'};
 
 const templateDirectory = 'src/templates';
+const partialsDirectory = 'src/templates/partials';
 const outputDirectory = 'public';
 
-fs.readdir(templateDirectory, (err, files) => {
-    if (err) {
-        console.error(err);
-        return;
+let partials = {};
+
+const partialsDirectoryFiles = fs.readdirSync(partialsDirectory);
+if (!partialsDirectoryFiles || !partialsDirectoryFiles.length) {
+    console.error('could not file partials files');
+    process.exit(0);
+}
+
+// Find all the mustache partials in the partils directory and add them to the partials
+// object which eventually is passed to Mustache.render()
+partialsDirectoryFiles.forEach((fileName) => {
+    const filePath = path.join(partialsDirectory, fileName);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isFile() && fileName.indexOf('.mustache') > -1) {
+        const partialPathInformation = path.parse(filePath);
+        const fileContents = fs.readFileSync(filePath, 'utf8');
+        partials[partialPathInformation.name] = fileContents;
     }
+});
 
-    files.forEach((fileName) => {
-        const filePath = path.join(templateDirectory, fileName);
-        fs.stat(filePath, (err, stats) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
 
-            // Find all the mustache files in the template directory and output them to files
-            if (stats.isFile() && fileName.indexOf('.mustache') > -1) {
-                outputTemplateToFile(filePath);
-            }
-        });
-    });
+const templateDirectoryFiles = fs.readdirSync(templateDirectory);
+if (!templateDirectoryFiles || !templateDirectoryFiles.length) {
+    console.error('could not file template files');
+    process.exit(0);
+}
+
+templateDirectoryFiles.forEach((fileName) => {
+    const filePath = path.join(templateDirectory, fileName);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isFile() && fileName.indexOf('.mustache') > -1) {
+        outputTemplateToFile(filePath);
+    }
 });
 
 function outputTemplateToFile(pathToTemplate) {
@@ -35,10 +51,8 @@ function outputTemplateToFile(pathToTemplate) {
             return;
         }
 
-        console.log(templateContents)
-
         const parsedTemplate = Mustache.parse(templateContents);
-        const outputToWrite = Mustache.render(parsedTemplate, siteData);
+        const outputToWrite = Mustache.render(templateContents, siteData, partials);
         const templatePathInformation = path.parse(pathToTemplate);
         const pathToOutputFile = path.join(outputDirectory, `${templatePathInformation.name}.html`);
 
